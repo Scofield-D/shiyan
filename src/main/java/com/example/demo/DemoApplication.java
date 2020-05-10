0e9c45c3f91e603253e03a78d8280bcebe45e1b1
Scofield <154411698@qq.com>
2020-05-10 20:58:09 +0800

    commit init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        // master branch
+        System.out.println("This is a master branch.");
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
